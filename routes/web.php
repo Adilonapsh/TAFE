cf2f480f0947211f637c0cf5f2aaa4468cd95d8a
Adilonapsh <Lubos418@gmail.com>
2020-10-14 13:41:39 +0700

    Admin Lte

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('Nodemcu', function () {
+    return view('nodemcu');
 });
 
 Auth::routes();
